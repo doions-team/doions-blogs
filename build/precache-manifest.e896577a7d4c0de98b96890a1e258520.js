ce2d6fa358b1d511993a74d8c9a7b7dcfae30209
Rupam Kairi <rupamkairi@hotmail.com>
2021-04-23 14:51:41 +0530

    content update

---
+++ build/precache-manifest.e896577a7d4c0de98b96890a1e258520.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68f3dc90ee52a72b74f770ade3b3e90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30240d09125a1b3e90ac",
+    "url": "/static/css/3.b5ebc86c.chunk.css"
+  },
+  {
+    "revision": "20e8c2bd2e032ae4f49d",
+    "url": "/static/css/4.8c6ad557.chunk.css"
+  },
+  {
+    "revision": "80788d04e9244c0e1350",
+    "url": "/static/css/5.b5ebc86c.chunk.css"
+  },
+  {
+    "revision": "e0a493ece864baf9c469",
+    "url": "/static/css/8.e2e5e525.chunk.css"
+  },
+  {
+    "revision": "8cedbc9f3a5099129a2a",
+    "url": "/static/css/main.be48df2f.chunk.css"
+  },
+  {
+    "revision": "0f32ab2121d247135cd8",
+    "url": "/static/js/2.8d88b1e4.chunk.js"
+  },
+  {
+    "revision": "30240d09125a1b3e90ac",
+    "url": "/static/js/3.6bf2aa30.chunk.js"
+  },
+  {
+    "revision": "20e8c2bd2e032ae4f49d",
+    "url": "/static/js/4.e5c46d0e.chunk.js"
+  },
+  {
+    "revision": "80788d04e9244c0e1350",
+    "url": "/static/js/5.1d817a78.chunk.js"
+  },
+  {
+    "revision": "21ff091941dc3b212013",
+    "url": "/static/js/6.ea805b8e.chunk.js"
+  },
+  {
+    "revision": "cf5bdd9ab68b874ebb4c",
+    "url": "/static/js/7.f7c4db14.chunk.js"
+  },
+  {
+    "revision": "e0a493ece864baf9c469",
+    "url": "/static/js/8.d68d81ce.chunk.js"
+  },
+  {
+    "revision": "e2d55578d63c81f63691",
+    "url": "/static/js/9.54284620.chunk.js"
+  },
+  {
+    "revision": "8cedbc9f3a5099129a2a",
+    "url": "/static/js/main.fe8f026b.chunk.js"
+  },
+  {
+    "revision": "b39077d7566ee60a310b",
+    "url": "/static/js/runtime~main.abb543e3.js"
+  },
+  {
+    "revision": "4d7ca05f145f70fc4892da8593725ccd",
+    "url": "/static/media/avatar.4d7ca05f.jpg"
+  },
+  {
+    "revision": "e86cb8875c926d0f972a91b8ef00eb28",
+    "url": "/static/media/avatar.e86cb887.jpg"
+  },
+  {
+    "revision": "0944e10bcec57cc548fe185c32c6bc6b",
+    "url": "/static/media/cover.0944e10b.svg"
+  },
+  {
+    "revision": "590925aa53120c8b752bc1c3946c63f8",
+    "url": "/static/media/cover.590925aa.jpg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/static/media/facebook-square-brands.34e73505.svg"
+  },
+  {
+    "revision": "8b847adc24081a47943c63433a46a967",
+    "url": "/static/media/icon.8b847adc.svg"
+  },
+  {
+    "revision": "712acef58607e92ad4316178d9c71a63",
+    "url": "/static/media/image-1.712acef5.jpg"
+  },
+  {
+    "revision": "88336a282bdc1ef764963250c44b5e5e",
+    "url": "/static/media/image-1.88336a28.jpg"
+  },
+  {
+    "revision": "b25e52ca2164be406cc8ee10a097e230",
+    "url": "/static/media/image-2.b25e52ca.jpg"
+  },
+  {
+    "revision": "db4b76ad40dca5171592c6fa60c2e5f8",
+    "url": "/static/media/image-2.db4b76ad.jpg"
+  },
+  {
+    "revision": "5015a3a77b3b73a47fa6d8309a5d198c",
+    "url": "/static/media/image-3.5015a3a7.jpg"
+  },
+  {
+    "revision": "417eb04f67044250e511ef60cdf53f74",
+    "url": "/static/media/image-4.417eb04f.jpg"
+  },
+  {
+    "revision": "5a8c2d495c7de3fbcb293b93f61f4249",
+    "url": "/static/media/image-5.5a8c2d49.jpg"
+  },
+  {
+    "revision": "899769b2e408fae95a412ae354803df9",
+    "url": "/static/media/image-6.899769b2.jpg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/static/media/instagram-brands.e75317fa.svg"
+  }
+]);
